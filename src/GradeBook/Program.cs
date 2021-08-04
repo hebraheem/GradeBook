f75431a1889f0fa7458cf70c2d91b1b4d428642e
hebraheem <iadebiyi01.com>
2021-08-04 18:20:45 +0100

    refactor code

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,23 +6,37 @@ namespace GradeBook
     class Program
     {
         static int count = 1;
-        static void GradeBookList()
+        static void Main(string[] args)
         {
-            InMemoryBook book = EnterBook();
+            IBook book = new InMemoryBook("hebraheem book");
+            book.GradeAdded += onGradedAdded;
 
+            EnterBook(book);
             var result = book.GetGradeStats();
 
             Console.WriteLine($"the average grade is {result.Average:N3}");
             Console.WriteLine($"the highest grade is {result.High}");
             Console.WriteLine($"the lowest grade is {result.Low}");
             Console.WriteLine($"the letter grade is {result.Letter}");
+
         }
+        // IBook book = new InMemoryBook("hebraheem book");
+        // book.GradeAdded += onGradedAdded
+        // static int count = 1;
+        // static void GradeBookList()
+        // {
+        //     InMemoryBook book = EnterBook(IBook book);
 
-        private static InMemoryBook EnterBook()
+            // var result = book.GetGradeStats();
+
+            // Console.WriteLine($"the average grade is {result.Average:N3}");
+            // Console.WriteLine($"the highest grade is {result.High}");
+            // Console.WriteLine($"the lowest grade is {result.Low}");
+            // Console.WriteLine($"the letter grade is {result.Letter}");
+        // }
+
+        private static IBook EnterBook(IBook book)
         {
-            var book = new InMemoryBook("hebraheem book");
-            book.GradeAdded += onGradedAdded;
-
             while (true)
             {
                 Console.WriteLine("Pls Enter a valid grade or q to compute grade");
@@ -50,18 +64,12 @@ namespace GradeBook
 
                 }
             }
-
             return book;
         }
 
         static void onGradedAdded(object sender, EventArgs e)
         {            
             Console.WriteLine($"{count++} Grade Added");
-        }
-
-        static void Main(string[] args)
-        {
-            GradeBookList();
         }
     }
 
